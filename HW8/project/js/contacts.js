d50cb552c3f8b3f6de07bf7b2b6b93dacc535a25
Desislava Hristova <d.hristovaddx@gmail.com>
2018-09-12 23:33:19 +0300

    HW8

---
+++ HW8/project/js/contacts.js
@@ -0,0 +1,7 @@
+var contacts = [
+    {firstName: 'Иван', lastName: 'Георгиев', email: 'ig@gmail.com', contactType: '2'},
+    {firstName: 'Елица', lastName: 'Димитрова', email: 'ed@gmail.com', contactType: '1'},
+    {firstName: 'Стефан', lastName: 'Христов', email: 'sh@gmail.com', contactType:'2'},
+    {firstName: 'Георги', lastName: 'Петров', email: 'gp@gmail.com', contactType:'1'},
+    {firstName: 'Петя', lastName: 'Людмилова', email: 'pl@gmail.com', contactType:'2'}
+];
